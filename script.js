bd047493bf8c27e830a544ba79bd2759cae502eb
tanavishali <tanawishalrai5271@gmail.com>
2024-08-07 11:58:31 +0500

    project practice

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-document.getElementById("demo").textContent = "hello";
-let print = (document.getElementById("demo").textContent = "hello");
-console.log(print);
+// document.getElementById("demo").textContent = "hello";
+// let print = (document.getElementById("demo").textContent = "hello");
+// console.log(print);
+// document.write();
+// print = "i am tanawish";
+
+// document.write("i ma tanawish");
+// console.log(document.baseURI);
+// console.log(document.body);
+// console.log(document.cookie);
+// console.log(document.doctype);
+// console.log(document.documentElement);
+// console.log(document.documentMode);
+// console.log(document.documentURI);
+// console.log(document.domain);
+// console.log(document.scripts);
+// console.log(document.URL);
+// const x = document.querySelectorAll(".intro");
+// console.log(x);
+// document.write(Date());
+// let showdate = (document.getElementById("demo2").innerHTML =
+//   "Date : " + Date());
+// console.log(showdate);
+// function getvaluebyid(value) {
+//   let getvalue = document.getElementById("text1").value;
+//   console.log(getvalue);
+//   document.getElementById("demovalue").textContent = getvalue;
+// }
